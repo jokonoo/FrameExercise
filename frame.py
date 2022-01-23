9b7bec7cf8313454f8b41fafff03f96400a70c12
Kuba Wojdas <kubaw9@onet.eu>
2022-01-23 01:03:52 +0100

    Changed def to class with build method

--- frame.py
+++ frame.py
@@ -1,20 +1,44 @@
 from math import ceil
 
 
-def frame(text, char, number_of_spaces=1, center=False):
-    number_of_spaces = ceil(abs(number_of_spaces))  # making sure that number is both positive and not int
-    longest_word = len(max(text, key=len))  # finding length of the longest word
-    floor = (longest_word + 2 + (2 * number_of_spaces)) * char  # building floor based on len of longest word and character each side
-    if center:
-        wall = [
-            f'{char}{word.center(len(floor) - 2 * len(char))}{char}'
-            for word in text]  # if center is set to True, then words are centered
-    else:
-        wall = [
-            f'{char}{word.rjust(len(word) + number_of_spaces)}{char.rjust(len(floor) - (len(char)) - number_of_spaces - len(word))}'
-            for word in text]  # building walls based on length of longest word, number of spaces, and character/s
-    wall = '\n'.join(wall)  # joining all words with newline character
-    return f'{floor}\n{wall}\n{floor}'  # finally returning whole frame together
+class CharLengthError(Exception):
+    pass
 
 
-print(frame(['Test', 'Small', 'Words'], '???'))
+class NotEvenOrOddNumberOfCharacters(Exception):
+    pass
+
+
+class Frame:
+    def __init__(self, text, char, number_of_spaces=1, centre=False):
+        self.text = text
+        if len(char) > 1:
+            raise CharLengthError('Must be only one character')
+        self.char = char
+        self.number_of_spaces = ceil(abs(number_of_spaces))
+        if centre:
+            if all([True if len(word) % 2 == 0 else False for word in text]):
+                self.centre = centre
+            elif all([True if len(word) % 2 != 0 else False for word in text]):
+                self.centre = centre
+            else:
+                raise NotEvenOrOddNumberOfCharacters('Must be only even or odd number of characters in words to center')
+
+    def build_frame(self):
+        n_of_spaces = ceil(abs(self.number_of_spaces))  # making sure that number is both positive and not int
+        longest_word = len(max(self.text, key=len))  # finding length of the longest word
+        floor = (longest_word + 2 + (
+                    2 * n_of_spaces)) * self.char  # building floor based on len of longest word and character each side
+        if self.centre:
+            wall = [
+                f'{self.char}{word.center(len(floor) - 2 * len(self.char))}{self.char}'
+                for word in self.text]  # if center is set to True, then words are centered
+        else:
+            wall = [
+                f'{self.char}{word.rjust(len(word) + n_of_spaces)}{self.char.rjust(len(floor) - (len(self.char)) - n_of_spaces - len(word))}'
+                for word in self.text]  # building walls based on length of longest word and number of spaces
+        wall = '\n'.join(wall)  # joining all words with newline character
+        return f'{floor}\n{wall}\n{floor}'  # finally returning whole frame together
+
+
+print(Frame(['AAAA', 'A', 'AAAAAAAAA'], '?', centre=True).build_frame())
--- tests.py
+++ tests.py
@@ -1,39 +1,34 @@
 import unittest
-from frame import frame
+from frame import Frame
 
 
 class TestFrame(unittest.TestCase):
     def test_basic(self):
-        self.assertEqual(frame(['Small', 'frame'], '~'), "~~~~~~~~~\n~ Small ~\n~ frame ~\n~~~~~~~~~")
-        self.assertEqual(frame(['Create', 'this', 'kata'], '+'), '++++++++++\n+ Create +\n+ this   +\n+ kata   '
+        self.assertEqual(Frame(['Small', 'frame'], '~').build_frame(), "~~~~~~~~~\n~ Small ~\n~ frame ~\n~~~~~~~~~")
+        self.assertEqual(Frame(['Create', 'this', 'kata'], '+').build_frame(), '++++++++++\n+ Create +\n+ this   +\n+ kata   '
                                                                  '+\n++++++++++')
-        self.assertEqual(frame(['This is a very long single frame'], '-'), '------------------------------------\n- '
+        self.assertEqual(Frame(['This is a very long single frame'], '-').build_frame(), '------------------------------------\n- '
                                                                            'This is a very long single frame '
                                                                            '-\n------------------------------------')
-        self.assertEqual(frame(
+        self.assertEqual(Frame(
             [' Create a frame!', '          __     __', '         /  \\~~~/  \\', '   ,----(     ..    )',
-             '  /      \\__     __/', ' /|         (\\  |(', '^  \\  /___\\  /\\ |', '   |__|   |__|-..'], '*'),
+             '  /      \\__     __/', ' /|         (\\  |(', '^  \\  /___\\  /\\ |', '   |__|   |__|-..'], '*').build_frame(),
             "*************************\n*  Create a frame!      *\n*           __     __   *\n*          "
             "/  \\~~~/  \\  *\n*    ,----(     ..    ) *\n*   /      \\__     __/  *\n*  /|         (\\  "
             "|(    *\n* ^  \\  /___\\  /\\ |     *\n*    |__|   |__|-..     *\n*************************")
 
     def test_multiple_spaces(self):
-        self.assertEqual(frame(['Test', 'Small', 'Words'], '.', 2), '...........\n.  Test   .\n.  Small  .\n.  Words  '
+        self.assertEqual(Frame(['Test', 'Small', 'Words'], '.', 2).build_frame(), '...........\n.  Test   .\n.  Small  .\n.  Words  '
                                                                     '.\n...........')
-        self.assertEqual(frame(['Test', 'Small', 'Words'], '.', 0), '.......\n.Test .\n.Small.\n.Words.\n.......')
-        self.assertEqual(frame(['Test', 'Small', 'Words'], '.', -2), '...........\n.  Test   .\n.  Small  .\n.  Words  '
+        self.assertEqual(Frame(['Test', 'Small', 'Words'], '.', 0).build_frame(), '.......\n.Test .\n.Small.\n.Words.\n.......')
+        self.assertEqual(Frame(['Test', 'Small', 'Words'], '.', -2).build_frame(), '...........\n.  Test   .\n.  Small  .\n.  Words  '
                                                                      '.\n...........')
-        self.assertEqual(frame(['Test', 'Small', 'Words'], '.', -1.5),
+        self.assertEqual(Frame(['Test', 'Small', 'Words'], '.', -1.5).build_frame(),
                          '...........\n.  Test   .\n.  Small  .\n.  Words  '
                          '.\n...........')
 
-    def test_long_characters(self):
-        self.assertEqual(frame(['Test', 'Small', 'Words'], '??'),
-                         '??????????????????\n?? Test         ??\n?? Small        ??\n?? Words        '
-                         '??\n??????????????????')
-        self.assertEqual(frame(['Test', 'Small', 'Words'], '???'), '???????????????????????????\n??? Test             '
-                                                                   '   ???\n??? Small               ???\n??? Words   '
-                                                                   '            ???\n???????????????????????????')
+    def test_centre(self):
+        self.assertEqual()
 
 
 if __name__ == '__main__':
